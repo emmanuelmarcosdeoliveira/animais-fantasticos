e458bfff81336a8de8ea60c92451a0dc97b2314e
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-05-27 20:13:39 -0300

    refactor:anima-numeros.js
    reviewed: Emmanuel Oliveira

--- scripts/modules/anima-numeros.js
+++ scripts/modules/anima-numeros.js
@@ -1,28 +1,50 @@
-export default function initAnimaNumeros() {
-  function aninaNumeros() {
-    const numeros = document.querySelectorAll('[data-numero]')
-    numeros.forEach((numeros) => {
-      let start = 0
-      const total = +numeros.innerText
-      const timer = setInterval(() => {
-        const incremento = Math.floor(total / 100)
-        start = start + incremento
-        numeros.innerText = start
-        if (start > total) {
-          numeros.innerText = total
-          clearInterval(timer)
-        }
-      }, 50 * Math.random())
-    })
-  }
-  function handleMutation(mutation) {
-    if (mutation[0].target.classList.contains('active')) {
-      observer.disconnect()
-      aninaNumeros()
-    }
+export default class AnimaNumeros {
+  constructor(numeros, observerTarget, observerClass) {
+    this.numeros = document.querySelectorAll(numeros)
+    this.observerTarget = document.querySelector(observerTarget)
+    this.observerClass = observerClass
+    // bind o this do objeto  ao callback da mutaçào
+    this.handleMutation = this.handleMutation.bind(this)
   }
 
-  const observerTarget = document.querySelector('.numeros')
-  const observer = new MutationObserver(handleMutation)
-  observer.observe(observerTarget, { attributes: true })
+  // Recebe um elmento do Dom com numero em seu texto
+  // Incrementa a partir de 0 até o numero final
+  static incrementarNumero(numero) {
+    const total = +numero.innerText
+    const incremento = Math.floor(total / 100)
+    let start = 0
+    const timer = setInterval(() => {
+      start += incremento
+      numero.innerText = start
+      if (start > total) {
+        numero.innerText = total
+        clearInterval(timer)
+      }
+    }, 25 * Math.random())
+  }
+
+  // Ativa incrementar numero para cada
+  // Numero selecionado do Dom
+  aninaNumeros() {
+    this.numeros.forEach((numero) => this.constructor.incrementarNumero(numero))
+  }
+  // Funçào que ocorre quando a mutação ocorrer
+  handleMutation(mutation) {
+    if (mutation[0].target.classList.contains(this.observerClass)) {
+      this.observer.disconnect()
+      this.aninaNumeros()
+    }
+  }
+  //Adiciona o MutationObserver para verificar
+  // Quando a classe active é adicionada ao elemento target
+  addMutationObserver() {
+    this.observer = new MutationObserver(this.handleMutation)
+    this.observer.observe(this.observerTarget, { attributes: true })
+  }
+  init() {
+    if (this.numeros.length && this.observerTarget) {
+      this.addMutationObserver()
+    }
+    return this
+  }
 }
--- scripts/modules/fetch-animais.js
+++ scripts/modules/fetch-animais.js
@@ -1,26 +1,31 @@
-import initAnimaNumeros from './anima-numeros.js'
+import AnimaNumeros from "./anima-numeros.js"
 
 export default function initFetchAnimais() {
   async function fetchAnimais(url) {
     try {
       const animaisResponse = await fetch(url)
       const animaisJSON = await animaisResponse.json()
-      const numerosGrid = document.querySelector('.numeros-grid')
+      const numerosGrid = document.querySelector(".numeros-grid")
       animaisJSON.forEach((animal) => {
         const divAnimal = createAnimal(animal)
         numerosGrid.appendChild(divAnimal)
       })
-      initAnimaNumeros()
+      const animaNumeros = new AnimaNumeros(
+        "[data-numero]",
+        ".numeros",
+        "active"
+      )
+      animaNumeros.init()
     } catch (erro) {
       console.log(erro)
     }
   }
 
   function createAnimal(animal) {
-    const div = document.createElement('div')
-    div.classList.add('numero-animal')
+    const div = document.createElement("div")
+    div.classList.add("numero-animal")
     div.innerHTML = `<h3>${animal.specie}</h3><span data-numero>${animal.total}</span>`
     return div
   }
-  fetchAnimais('./animaisapi.json')
+  fetchAnimais("./animaisapi.json")
 }
