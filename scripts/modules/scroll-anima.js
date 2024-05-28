f06450a1973e13d6e2a390f393e051d6d181919c
Emmanuel Marcos de  Oliveira <116108389+emmanuelmarcosdeoliveira@users.noreply.github.com>
2024-05-28 09:57:21 -0300

    Refactor animaScroll (#11)
    reviwed: Emmanuel Oliveira

---
+++ scripts/modules/scroll-anima.js
@@ -0,0 +1,42 @@
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections)
+    this.windowMetade = window.innerHeight * 0.5
+    this.checkDistance = this.checkDistance.bind(this)
+  }
+  // Pega a distancia de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade)
+      }
+    })
+  }
+  //Verifica a distancia em cada objeto em
+  // em relaçao ao site
+
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add("active")
+      } else if (item.element.classList.contains("active")) {
+        item.element.classList.remove("active")
+      }
+    })
+  }
+
+  init() {
+    if (this.sections.length) {
+      this.getDistance()
+      this.checkDistance()
+      window.addEventListener("scroll", this.checkDistance)
+    }
+    return this
+  }
+  // Remove o evento de scroll
+  toDestroy() {
+    window.removeEventListener("scroll", this.checkDistance)
+  }
+}
