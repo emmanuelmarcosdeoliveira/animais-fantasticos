be67b97dca92d5c4df7ba12af677f694448759c9
Emmanuel Marcos de Oliveira <emmanuelmarcosdeoliveira@gmail.com>
2024-05-28 09:54:13 -0300

    refactor: improving the performance of srcoll-anima.js
    reviewed: Emmanuel Oliveira

--- scripts/modules/scroll-anima.js
+++ scripts/modules/scroll-anima.js
@@ -1,24 +1,42 @@
 export default class ScrollAnima {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections)
-    this.windowMetade = window.innerHeight * 0.6
-    this.animaScroll = this.animaScroll.bind(this)
+    this.windowMetade = window.innerHeight * 0.5
+    this.checkDistance = this.checkDistance.bind(this)
   }
+  // Pega a distancia de cada item em relação ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade)
+      }
+    })
+  }
+  //Verifica a distancia em cada objeto em
+  // em relaçao ao site
 
-  animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top
-      const isSectionVisible = sectionTop - this.windowMetade < 0
-      if (isSectionVisible) {
-        section.classList.add("active")
-      } else if (section.classList.contains("active")) {
-        section.classList.remove("active")
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offset) {
+        item.element.classList.add("active")
+      } else if (item.element.classList.contains("active")) {
+        item.element.classList.remove("active")
       }
     })
   }
 
   init() {
-    this.animaScroll()
-    window.addEventListener("scroll", this.animaScroll)
+    if (this.sections.length) {
+      this.getDistance()
+      this.checkDistance()
+      window.addEventListener("scroll", this.checkDistance)
+    }
+    return this
+  }
+  // Remove o evento de scroll
+  toDestroy() {
+    window.removeEventListener("scroll", this.checkDistance)
   }
 }
